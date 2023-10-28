dcb5549525f47cba73ee22a6b5fb7e17a90cc5e4
obsiddev <jo.piechl@outlook.com>
2023-10-28 17:50:39 +0200

    changed info

--- client.js
+++ client.js
@@ -38,32 +38,28 @@ function sendMessage() {
 
 // if connection was sucessful
 function Connected() {
-    console.log("connected to " + hostname + ":" + port + " with clientId " + clientId);
-    document.getElementById("logger").innerHTML = "connected with ID" + idNum;
+    console.log("connected to " + hostname + ":" + port + " with id " + clientId);
+    document.getElementById("statusText").innerHTML = "connected"
+    document.getElementById("idText").innerHTML = "id: " + idNum
     mqttClient.subscribe("metrochat/main");
 }
 
 // if connection is failed
 function ConnectionFailed(res) {
     console.log("Connect failed:" + res.errorMessage);
-    document.getElementById("logger").innerHTML = "failed to connect: " + res.errorMessage;
+    document.getElementById("statusText").innerHTML = "failed to connect: " + res.errorMessage
 }
 
 // if connection is lost
 function ConnectionLost(res) {
     if (res.errorCode != 0) {
         console.log("Connection lost:" + res.errorMessage);
-        document.getElementById("logger").innerHTML = "connection lost: " + res.errorMessage;
+        document.getElementById("statusText").innerHTML = "connection lost: " + res.errorMessage
         Connect();
     }
 }
 
 // message handling
 function MessageArrived(message) {
-    if (document.getElementById("content").innerHTML == "nothing to see here") {
-        document.getElementById("content").innerHTML = message.payloadString + "<br>";
-    }
-    else {
-        document.getElementById("content").innerHTML += message.payloadString + "<br>";
-    }
+    document.getElementById("content").innerHTML += message.payloadString + "<br>";
 }
